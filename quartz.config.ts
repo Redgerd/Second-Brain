9d302651666400ebff85be171c4da6ddd3914021
Redgerd <117646793+Redgerd@users.noreply.github.com>
2026-06-10 17:36:28 +0500

    Setting base url for correct preview page

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "redgerd.github.io/Second-Brain",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
